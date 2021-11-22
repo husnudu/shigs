5cdc672ea7b791b5080901096cea96dad176f1fe
Sandro Putraa <59155826+sandrocods@users.noreply.github.com>
2021-11-22 23:24:42 +0700

    Update main.py
    Delete Bot Token

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 requests.packages.urllib3.disable_warnings(
     requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
-bot = telebot.TeleBot("1958392182:AAGWEOLF4YIvsIiLvpz-BOlJOGPb6IRYJow",
+bot = telebot.TeleBot("",
                       parse_mode="HTML")  # You can set parse_mode by default. HTML or MARKDOWN
 
 
